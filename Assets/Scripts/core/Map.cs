0cec02f98798a616b4b162e7c5f56a405d16f326
yanjw <dupouy2008@gmail.com>
2017-08-04 01:32:16 +0800

    Map class construct

--- Assets/Scripts/core/Map.cs
+++ Assets/Scripts/core/Map.cs
@@ -2,8 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 namespace Assets.Scripts.core {
     class Map {
+        const float VIEWPORT_WIDTH = 10f;
+        const float VIEWPORT_HEIGHT = 10f;
+        const float BUFF_LENGTH = 0f;
+        // 视窗
+        readonly Rect viewport = new Rect(0,0, VIEWPORT_WIDTH,VIEWPORT_HEIGHT);
+
+        readonly Rect renderRect = new Rect();
+        readonly Dictionary<string, GameObject[]> canvas = new Dictionary<string, GameObject[]>();
+        readonly Vector2 helpPoint = new Vector2();
+
+        MAP_DATA data;
+
+        public Map() {
+        }
+
+        public void load() {
+        }
+
+        public void save() {
+        }
+
+        void render() {
+            renderRect.Set(
+                Mathf.Floor(viewport.x / MAP_DATA.TILE_WIDTH),
+                Mathf.Floor(viewport.y / MAP_DATA.TILE_HEIGHT),
+                Mathf.Ceil(viewport.xMax / MAP_DATA.TILE_WIDTH),
+                Mathf.Ceil(viewport.yMax / MAP_DATA.TILE_HEIGHT)
+                );
+
+            foreach (string key in canvas.Keys) {
+                string[] keys = key.Split('_');
+                helpPoint.Set(System.Convert.ToSingle(keys[0]), System.Convert.ToSingle(keys[1]));
+            }
+        }
+
+        void renderTile(Vector3 [] data) {
+        }
+
+        void removeTile(GameObject [] bricks) {
+        }
+    }
+
+    public struct MAP_DATA {
+        public const float TILE_WIDTH = 10f;
+        public const float TILE_HEIGHT = 10f;
+        public Dictionary<string, Vector3 []> tiles;
     }
 }
--- Assets/Scripts/core/TerrainSpawner.cs
+++ Assets/Scripts/core/TerrainSpawner.cs
@@ -7,9 +7,32 @@ using System.Collections;
 
 namespace Assets.Scripts.core {
     class TerrainSpawner {
-        public void spawn(Transform center) {
-            GameObject brick = GameObject.FindWithTag("brick");
-            GameObject.Instantiate(brick, center, true);
+        const float MAX_Y_SCALE = 5f;
+        const float SPAWNER_PROBABILITY = 0.6f;
+        const String BRICK_PATH = "Perfab/brick";
+
+        GameObject brick;
+        Transform stage;
+
+        public TerrainSpawner(Transform stage) {
+            brick = Resources.Load(BRICK_PATH) as GameObject;
+            this.stage = stage;
+        }
+
+        public void spawn(Rect rect) {
+            // 遍历矩形区域，生成地形
+            for (float x = rect.x; x < rect.xMax; x++) {
+                for (float z = rect.y; z < rect.yMax; z++) {
+                    float p = UnityEngine.Random.Range(0, 1);
+                    if (p > SPAWNER_PROBABILITY) {
+                        GameObject tile = GameObject.Instantiate(brick, stage, false) as GameObject;
+                        // 随机 y 长度
+                        float yScale = UnityEngine.Random.Range(1, MAX_Y_SCALE);
+                        // 设置 tile 的坐标
+                        tile.transform.InverseTransformPoint(x, yScale * 0.5f, z);
+                    }
+                }
+            }
         }
     }
 }
