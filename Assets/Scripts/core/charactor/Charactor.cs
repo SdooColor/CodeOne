98fff7be3c2b1fd7a79e131616e0a26d7776ac21
yanjw <dupouy2008@gmail.com>
2017-09-01 01:53:03 +0800

    移动控制交由 charactor 实现

--- Assets/Scripts/core/charactor/Charactor.cs
+++ Assets/Scripts/core/charactor/Charactor.cs
@@ -5,28 +5,46 @@ public class Charactor : MonoBehaviour {
     // current mode's forward
     public Vector3 forward;
     public Vector3 up;
-    public float runSpeed = 2;
 
+    public float runSpeed = 2;
     public Animator animator;
 
-    public GameObject mode;
+    public Vector3 currentSpeedV;
 
 	// Use this for initialization
 	void Start () {
         animator = GetComponent<Animator>();
-        mode = gameObject;
         forward = transform.forward;
         up = transform.forward;
+        currentSpeedV = Vector3.zero;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	}
+        basicMoveFoo();
+    }
+
+    // it's a basic move controll
+    // 能被更高优先级的动作控制其执行开关
+    void basicMoveFoo() {
+        float zV = Input.GetAxis("Vertical");
+        float xV = Input.GetAxis("Horizontal");
+        if (xV != 0 || zV != 0) {
+            animator.SetBool("move", true);
+
+            Quaternion rotation = Quaternion.LookRotation(new Vector3(xV, 0, zV), transform.up);
+            transform.LookAt(rotation * forward + transform.position);
+            transform.localPosition = rotation * (forward * runSpeed * Time.deltaTime) + transform.position;
+        }
+        else {
+            animator.SetBool("move", false);
+            transform.LookAt(forward + transform.position);
+        }
+    }
 
     public void updateForward(Vector3 forward) {
         transform.rotation = Quaternion.LookRotation(forward);
         this.forward.Set(forward.x, forward.y, forward.z);
-        Debug.Log("updateForward:" + forward);
     }
 
     public int getState(int layer) {
